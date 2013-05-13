9e8b16e329af32544ef559f2ea1213de3aaaef6c
Clare <Clare@clarepc>
2013-05-13 22:55:24 +0800

    abstract dao

--- ejbModule/com/ecom/demo/dao/impl/DemoDAOImpl.java
+++ ejbModule/com/ecom/demo/dao/impl/DemoDAOImpl.java
@@ -4,20 +4,16 @@ import java.util.List;
 
 import javax.ejb.Local;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import com.ecom.common.dao.AbstractDAO;
 import com.ecom.demo.bo.Demo;
 import com.ecom.demo.dao.DemoDAO;
 
 @Stateless
 @Local(DemoDAO.class)
-public class DemoDAOImpl implements DemoDAO {
+public class DemoDAOImpl extends AbstractDAO implements DemoDAO {
 	
-	@PersistenceContext(unitName="ecommerce")
-	private EntityManager em;
-
 	@Override
 	public void insertDemo(Demo demo) {
 
